a60cf348cf411e06455f2bab416c5dcb73f28785
Ayamjt <a.majaiti@students.uu.nl>
2025-10-10 00:12:24 +0200

    metrics

---
+++ LearningApp/ProgramMetrics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ProgramMetrics
+{
+    public int NumberOfCommands { get; set; }
+    public int NumberOfRepeats { get; set; }
+    public int MaxNesting { get; set; }
+
+    public override string ToString()
+    {
+        return $"Commands: {NumberOfCommands}, Repeats: {NumberOfRepeats}, Max nesting: {MaxNesting}";
+    }
+}
+
